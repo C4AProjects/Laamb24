f7a28ebad158db889996f4f5cf572c7fdb08ad04
Ousmaneo <ousmanesamba@gmail.com>
2013-12-10 12:10:32 +0000

    page simple

--- wordpress/wp-content/themes/dunk360/page.php
+++ wordpress/wp-content/themes/dunk360/page.php
@@ -7,9 +7,6 @@ global $post;
             <div class="page-head">
             	<div class="page-title"><h1><?php echo $post->post_title; ?></h1></div>
             </div>
-        </div>
-        <br class="clr" />
-        	<div class=" lft">
                <div class="main-content info-page">
     				<?php echo apply_filters('the_content', $post->post_content); ?>
                 </div>            
